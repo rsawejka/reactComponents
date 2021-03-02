8151f6ad032a42ffd45bf77a664d902b8363578b
rsawejka <rsawejka@my.wctc.com>
2021-03-02 17:55:13 -0600

    bug fixed

--- src/App.js
+++ src/App.js
@@ -7,19 +7,16 @@ function App() {
     //define state in functional component
     //     state,   setSate()            default value
     const [posts, setPosts] = useState([]);
-  return (
-    <div className="App">
-      <h1>
-          Our first react app.
-      </h1>
-
-      <Posts />
-
-        <SearchPosts  onSearchResult={(results) => setPosts(results)}/>
-        <hr />
-      <Posts  posts={posts}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>
+                Our first react app.
+            </h1>
+            <SearchPosts  onSearchResult={(results) => setPosts(results)}/>
+            <hr />
+            <Posts  posts={posts}/>
+        </div>
+    );
 }
 
 export default App;
