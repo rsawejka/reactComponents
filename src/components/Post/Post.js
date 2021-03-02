d0c8f6cb8682de80ad8234fcffa9aeb8eaacb750
rsawejka <rsawejka@my.wctc.com>
2021-03-02 11:42:37 -0600

    grid working

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,23 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Post.scss';
+import GridListTile from "@material-ui/core/GridListTile";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
 
 const Post = (props) => (
-  <div className="Post">
-          <h3>{props.title}</h3>
-          <p dangerouslySetInnerHTML={{__html: props.body}}></p>
+<div className="Post">
 
-  </div>
+
+        <GridListTile key="Subheader" cols={6} style={{ height: 'auto' }}>
+
+            <img className="img" src={props.featuredImg} />
+            <GridListTileBar
+                actionIcon={
+                    <img className="authorIcon" src={props.authorImg} />
+                }
+                title={props.title}
+                subtitle={<span>by: {props.author}</span>}
+
+            />
+        </GridListTile>
+
+</div>
+
 );
+
 
 Post.propTypes = {
     title: PropTypes.string,
-    body: PropTypes.string,
+    author: PropTypes.string,
+    authorImg: PropTypes.string,
+    featuredImg: PropTypes.string,
+
+
 };
 
 Post.defaultProps = {
     title: "post title",
-    body: "default body",
+    author: "default author",
+    authorImg: "img url",
+    featuredImg: "imgurl",
 };
 
 export default Post;
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './Posts.scss'
 import Post from '../Post/Post'
+import GridList from "@material-ui/core/GridList";
 
 
 
@@ -8,11 +9,19 @@ export class Posts extends Component {
 
     render() {
     return(
+
         <div className="posts">
+            <GridList cellHeight={180} className="grid">
             {this.props.posts.map((post) =>
-                  <Post title={post.title.rendered} body={post.content.rendered} key={post.id}/>
+                  <Post title={post.title.rendered} author={post._embedded.author[0].name}
+                        authorImg={post._embedded.author[0].avatar_urls[24]} key={post.id}
+                        featuredImg={post._embedded['wp:featuredmedia'][0].source_url}
+                  />
             )}
+            </GridList>
          </div>
+
+
         )
     }
 }
